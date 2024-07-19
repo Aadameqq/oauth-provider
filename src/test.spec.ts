7ba9ebb3e6ff0c30497213205f88e7b9fbe32c77
Aadameqq <adamek5599.sluzbowy@gmail.com>
2024-07-19 15:29:29 +0200

    feat: project configuration

---
+++ src/test.spec.ts
@@ -0,0 +1,11 @@
+describe('test', () => {
+    it('test', () => {
+        expect(true).toBe(false);
+    });
+    it('test2', () => {
+        expect(true).toBeTruthy();
+    });
+});
+describe('ed', () => {
+    expect('');
+});
